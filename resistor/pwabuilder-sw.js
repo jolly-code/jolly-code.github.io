9ba8d3f3db946165bd14e4fa04c73af442910a17
jamil <jamil@jollycode.net>
2018-06-22 22:58:50 -0400

    test doubled up sw

--- resistor/pwabuilder-sw.js
+++ resistor/pwabuilder-sw.js
@@ -16,7 +16,7 @@ self.addEventListener('install', function(event) {
 });
 
 //If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   var updateCache = function(request){
     return caches.open(cacheName).then(function (cache) {
       return fetch(request.clone()).then(function (response) {
@@ -26,7 +26,23 @@ self.addEventListener('fetch', event => {
     });
   };
 
-  updateCache(event.request);
+  event.waitUntil(updateCache(event.request));
+
+  event.respondWith(
+    fetch(event.request).catch(function(error) {
+      console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
+
+      //Check to see if you have it in the cache
+      //Return response
+      //If not in the cache, then return error page
+      return caches.open(cacheName).then(function (cache) {
+        return cache.match(event.request).then(function (matching) {
+          var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
+          return report
+        });
+      });
+    })
+  );
 })
 
 self.addEventListener('activate',  event => {
