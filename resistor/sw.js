6876150ff5c6f9cba772d62f5d4c482e6b06f9d0
jamil <jamil@jollycode.net>
2018-06-22 22:08:18 -0400

    test other sw

---
+++ resistor/sw.js
@@ -0,0 +1,31 @@
+var cacheName = 'hello-world-page-2';
+var filesToCache = [
+  '/resistor/',
+  '/resistor/index.htm',
+  '/resistor/style.css',
+  '/resistor/manup.js',
+  '/resistor/script.js',
+  '/resistor/shared.js'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
